be06f9d5da957aa3b0e0568594954d4e69b025bd
VieraGroup <Ray@vierainvestmentgroup.com>
2026-04-13 15:59:25 -0400

    feat: connect RelatedPosts to real post filtering

--- components/RelatedPosts.js
+++ components/RelatedPosts.js
@@ -1,10 +1,45 @@
+import { getCountyPosts, getCityPosts } from "../utils/relatedPosts";
+
 export default function RelatedPosts({
+  allPosts = [],
+  mode,
+  countySlug = "",
+  citySlug = "",
+  parentCountySlug = "",
   title = "Helpful Resources",
+  limit = null,
 }) {
+  let posts = [];
+
+  if (mode === "county" && countySlug) {
+    posts = getCountyPosts(allPosts, countySlug);
+  } else if (mode === "city" && citySlug) {
+    posts = getCityPosts(allPosts, citySlug, parentCountySlug);
+  }
+
+  if (limit) {
+    posts = posts.slice(0, limit);
+  }
+
+  if (!posts.length) {
+    return (
+      <section style={{ marginTop: "30px" }}>
+        <h2>{title}</h2>
+        <p>Related posts will appear here next.</p>
+      </section>
+    );
+  }
+
   return (
     <section style={{ marginTop: "30px" }}>
       <h2>{title}</h2>
-      <p>Related posts will appear here next.</p>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <a href={post.url}>{post.title}</a>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
