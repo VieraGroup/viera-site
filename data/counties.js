5c1c762c52895beca1beb0a1b96689c61f8549b2
VieraGroup <Ray@vierainvestmentgroup.com>
2026-04-13 15:45:18 -0400

    add root counties data file

---
+++ data/counties.js
@@ -0,0 +1,14 @@
+export const counties = [
+  {
+    slug: "passaic-county",
+    cities: ["clifton", "paterson", "passaic", "wayne", "pompton-lakes"],
+  },
+  {
+    slug: "bergen-county",
+    cities: ["hackensack", "paramus", "teaneck", "fair-lawn", "englewood"],
+  },
+  {
+    slug: "essex-county",
+    cities: ["newark", "bloomfield", "montclair", "irvington", "east-orange"],
+  },
+];
