8370d36a5cb39205fb20934495feccd8475a22c6
VieraGroup <Ray@vierainvestmentgroup.com>
2026-04-13 14:42:14 -0400

    fix: cleaned up folder structure and resolved duplicate file conflicts

--- pages/counties/passaic-county/index.js
+++
@@ -1,8 +0,0 @@
-export default function PassaicCountyPage() {
-  return (
-    <main style={{ padding: "40px", fontFamily: "Arial, sans-serif" }}>
-      <h1>Passaic County</h1>
-      <p>This is a test county page.</p>
-    </main>
-  );
-}
--- pages/counties/passaic-county/pages/test.js
+++
@@ -1,8 +0,0 @@
-export default function TestPage() {
-  return (
-    <main style={{ padding: "40px", fontFamily: "Arial, sans-serif" }}>
-      <h1>Test Page</h1>
-      <p>If you can see this, Next routes are working.</p>
-    </main>
-  );
-}
