f44836ac38bfb96330fcb575ac338ad44c9ad994
VieraGroup <Ray@vierainvestmentgroup.com>
2026-04-13 14:22:35 -0400

    add test page

---
+++ pages/counties/passaic-county/pages/test.js
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial, sans-serif" }}>
+      <h1>Test Page</h1>
+      <p>If you can see this, Next routes are working.</p>
+    </main>
+  );
+}
