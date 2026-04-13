38bd1fafc16e17503843498261ad345bab61d9fe
VieraGroup <Ray@vierainvestmentgroup.com>
2026-04-13 14:17:37 -0400

    add static passaic county test page

---
+++ pages/counties/passaic-county/index.js
@@ -0,0 +1,8 @@
+export default function PassaicCountyPage() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial, sans-serif" }}>
+      <h1>Passaic County</h1>
+      <p>This is a test county page.</p>
+    </main>
+  );
+}
