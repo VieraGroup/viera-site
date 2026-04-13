8f74427af26a1d3447cf643a7c95bc83229bfc15
VieraGroup <Ray@vierainvestmentgroup.com>
2026-04-13 13:58:17 -0400

    add next home page

---
+++ data/utils/components/pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial, sans-serif" }}>
+      <h1>Viera Investment Group LLC</h1>
+      <p>GitHub and Netlify are now connected.</p>
+      <p>The SEO system is being built step by step.</p>
+    </main>
+  );
+}
