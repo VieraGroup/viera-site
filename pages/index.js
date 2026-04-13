a7c6bdbf8c78d726c09dc3736e348b364a608864
VieraGroup <Ray@vierainvestmentgroup.com>
2026-04-13 15:09:36 -0400

    feat: rebuild clean homepage in pages folder

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial, sans-serif" }}>
+      <h1>Viera Investment Group</h1>
+      <p>Site is live 🔥</p>
+    </main>
+  );
+}
