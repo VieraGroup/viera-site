e71ec3706de2b14974c3e36aa54e552c4c958b44
VieraGroup <Ray@vierainvestmentgroup.com>
2026-04-13 15:45:45 -0400

    add root RelatedPosts component

---
+++ components/RelatedPosts.js
@@ -0,0 +1,10 @@
+export default function RelatedPosts({
+  title = "Helpful Resources",
+}) {
+  return (
+    <section style={{ marginTop: "30px" }}>
+      <h2>{title}</h2>
+      <p>Related posts will appear here next.</p>
+    </section>
+  );
+}
